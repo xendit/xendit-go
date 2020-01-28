8a28e5ed3453cc233a66bc7695718cda5700db14
slzhffktm <pinkappleberry98@gmail.com>
2020-01-28 16:27:09 +0700

    Fix comment

--- balance.go
+++ balance.go
@@ -10,7 +10,7 @@ const (
 	BalanceAccountTypeTax     BalanceAccountTypeEnum = "TAX"
 )
 
-// Balance contains data from Xendit's API response of balance related request.
+// Balance contains data from Xendit's API response of balance related requests.
 // For more details see https://xendit.github.io/apireference/?bash#balances.
 type Balance struct {
 	Balance float64 `json:"balance"`
