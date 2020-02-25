4389940211aca16a4a89a7f1a87a0e045d0c450a
slzhffktm <13516121@std.stei.itb.ac.id>
2020-02-25 13:05:36 +0700

    Add XAPIVersion and extra fields for ewallet

--- ewallet.go
+++ ewallet.go
@@ -12,14 +12,25 @@ const (
 	EWalletTypeLINKAJA EWalletTypeEnum = "LINKAJA"
 )
 
+// EWalletXApiVersionEnum constants are the available e-wallet "X-API-VERSION"s
+type EWalletXApiVersionEnum string
+
+// This consists the values that EWalletXApiVersionEnum can take
+const (
+	EWalletXApiVersionEnum20190204 EWalletXApiVersionEnum = "2019-02-04"
+	EWalletXApiVersionEnum20200201 EWalletXApiVersionEnum = "2020-02-01"
+)
+
 // EWallet contains data from Xendit's API response of e-wallet related requests.
 // For more details see https://xendit.github.io/apireference/?bash#ewallets.
 type EWallet struct {
-	EWalletType     EWalletTypeEnum `json:"ewallet_type"`
-	ExternalID      string          `json:"external_id"`
-	Status          string          `json:"status"`
-	Amount          float64         `json:"amount"`
-	TransactionDate *time.Time      `json:"transaction_date,omitempty"`
-	CheckoutURL     string          `json:"checkout_url,omitempty"`
-	BusinessID      string          `json:"business_id,omitempty"`
+	EWalletType          EWalletTypeEnum `json:"ewallet_type"`
+	ExternalID           string          `json:"external_id"`
+	Status               string          `json:"status"`
+	Amount               float64         `json:"amount"`
+	TransactionDate      *time.Time      `json:"transaction_date,omitempty"`
+	CheckoutURL          string          `json:"checkout_url,omitempty"`
+	BusinessID           string          `json:"business_id,omitempty"`
+	Created              *time.Time      `json:"created,omitempty"`
+	EWalletTransactionID string          `json:"e_wallet_transaction_id,omitempty"`
 }
