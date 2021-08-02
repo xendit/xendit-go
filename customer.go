f12b71007efa5ec183714898e5956f9542c7436e
Glenda Emanuella Sutanto <glendaes0103@gmail.com>
2021-08-02 11:45:56 +0700

    Add documentation for CustomerAddress

--- customer.go
+++ customer.go
@@ -1,5 +1,8 @@
 package xendit
 
+// CustomerAddress contains data from Xendit's API response of customer's Customer Addres requests.
+// For more details see https://xendit.github.io/apireference/?bash#customers.
+// For documentation of subpackage customer, checkout https://pkg.go.dev/github.com/xendit/xendit-go/customer/
 type CustomerAddress struct {
 	Country     string `json:"country" validate:"required"`
 	StreetLine1 string `json:"street_line1,omitempty"`
