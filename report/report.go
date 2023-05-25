95033b76e96e2c2a50048dc8dc3fdd49e0fc364f
zombozo12 <wigunaramadhan1998@gmail.com>
2023-05-25 14:44:19 +0700

    fix: add comments

--- report/report.go
+++ report/report.go
@@ -5,10 +5,12 @@ import (
 	"github.com/xendit/xendit-go"
 )
 
+// GenerateReport generates a report
 func GenerateReport(data *GenerateReportParams) (*xendit.Report, *xendit.Error) {
 	return GenerateReportWithContext(context.Background(), data)
 }
 
+// GenerateReportWithContext generates a report with context
 func GenerateReportWithContext(ctx context.Context, data *GenerateReportParams) (*xendit.Report, *xendit.Error) {
 	client, err := getClient()
 	if err != nil {
@@ -18,10 +20,12 @@ func GenerateReportWithContext(ctx context.Context, data *GenerateReportParams) 
 	return client.GenerateReportWithContext(ctx, data)
 }
 
+// GetReport gets a report
 func GetReport(data *GetReportParams) (*xendit.Report, *xendit.Error) {
 	return GetReportWithContext(context.Background(), data)
 }
 
+// GetReportWithContext gets a report with context
 func GetReportWithContext(ctx context.Context, data *GetReportParams) (*xendit.Report, *xendit.Error) {
 	client, err := getClient()
 	if err != nil {
